e68af911126d800a31fc7dd0e147b4a9e7d2a328
Kei Kondoh <kei@kondoh.asia>
2023-07-02 14:59:12 +0900

    エントリースクリプトを作成

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my-zsh-settings
+
+ZSHの設定をプラグインの機構を使って読み込ませるためのリポジトリ。
+
+[Sheldon](https://github.com/rossmacarthur/sheldon) での動作を確認している。
---
+++ my-zsh-settings.plugin.zsh
@@ -0,0 +1,29 @@
+#! /bin/zsh
+
+function __is_enabled() {
+    if printf '%s\n' "${ENABLEDAPP[@]}" | grep -qx $1; then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
+# カレントディレクトリを移動
+local orgpwd=$(pwd)
+cd $(dirname $0)
+
+# 環境変数 ENABLEDAPP が設定していなければすべてのアプリが入っている前提になる
+if [ -z "$ENABLEDAPP" ]; then
+    ENABLEDAPP=("fzf" "exa" "ghq")
+fi
+
+if [ $(__is_enabled "exa") -eq 1 ]; then
+    source setup-exa.zsh
+fi
+
+if [ $(__is_enabled "fzf") -eq 1 ] && [ $(__is_enabled "ghq") -eq 1 ]; then
+    source setup-fzf-ghq.zsh
+fi
+
+# カレントディレクトリを復帰
+cd $orgpwd
