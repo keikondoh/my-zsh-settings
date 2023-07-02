9f993f03db703d146135ab6102299c1914969359
Kei Kondoh <kei@kondoh.asia>
2023-07-02 14:59:32 +0900

    EXAのエイリアス設定を追加

---
+++ setup-exa.zsh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# Alias EXA
+alias ls='exa --icons -F'
+alias ll='exa --icons --git --header -l'
+alias lla='exa --icons --git --header -la'
+alias tree='exa --icons -T -D -I .git -I node_modules'
